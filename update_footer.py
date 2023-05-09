11ab7f1d8a2d876c1f0fee5043c7ae7a30a5bb68
Hopan <hopanz@gmail.com>
2023-05-09 23:52:51 +0700

    - Test for index

--- update_footer.py
+++ update_footer.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup, formatter
 import os
 
 def read_file_content(file_path):
-    f = open(file_path, "rb")
+    f = open(file_path, "r")
     content = f.read()
     f.close()
     return True, content
@@ -18,15 +18,41 @@ def update_file_footer(file_path):
     if not code:
         return False, "read_file_content error, " + str(content)
     
-    soup = BeautifulSoup(content, features="html.parser")
-    head_script = soup.select_one("head > script")
+    # content = content.decode("utf-8")
     
-    if head_script:
-        # Replace with new GA4
-        head_script.decompose()
+    # soup = BeautifulSoup(content, features="html.parser")
+    # head_script = soup.select_one("head > script")
+    
+    # if head_script:
+    #     # Replace with new GA4
+    #     head_script.decompose()
         
-    format = formatter.HTMLFormatter(indent=4)
-    content = soup.prettify(formatter=format)
+    # format = formatter.HTMLFormatter(indent=4)
+    # content = soup.prettify(formatter=format)
+    
+    # Check file edited
+    indicator = """</head>\n<script"""
+
+    insert_indicator = """</head>\n"""
+
+    google_analytic_code = """<script async src="https://www.googletagmanager.com/gtag/js?id=G-P9GG73NE6J"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-P9GG73NE6J');
+</script>
+"""
+    if content.find(indicator) == -1:
+        index = content.find(insert_indicator)
+        if index == -1:
+            return False, "Cannot found insert_indicator"
+        
+        index += len(insert_indicator)
+        
+        content = content[:index] + google_analytic_code + content[index:]
+    else:
+        return True, False
     
     write_file(file_path, content)
     
@@ -47,13 +73,13 @@ def update_all_footer():
             
             code, result = update_file_footer(full_path)
             if not code:
-                print("update_file_footer error, " + str(result))
+                print("update_file_footer error, %s - %s" % (full_path, str(result)))
                 return False, False
             
             count += 1
             
-            if count >= 1:
-                return True, False
+            # if count >= 2:
+            #     return True, False
 
     return True, True
 
